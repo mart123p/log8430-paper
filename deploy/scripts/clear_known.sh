5d139a9c4abc4c0d03e1d5e44c269cdf0630c2b6
Martin Pouliot <martinp507@gmail.com>
2020-11-13 15:11:12 -0500

    Added logic in the cleanup script

--- deploy/scripts/clear_known.sh
+++ deploy/scripts/clear_known.sh
@@ -1,10 +1,22 @@
-#!/bin/sh
+#!/bin/bash
 
 # Remove any old entry for the local port to avoid any issue. The main SSH
 # connection is still protected against mitm attacks. But local connections are
 # usually new VMs in a trusted network. There is little worry about.
 
-ssh-keygen -R "127.0.0.1:6052"
-ssh-keygen -R "127.0.0.1:6053"
-ssh-keygen -R "127.0.0.1:6054"
-ssh-keygen -R "127.0.0.1:6055"
+ip=$(cat ../creds/ip)
+worker=$(cat ../azure.yml | grep "amount_worker:" | xargs | cut -d ' ' -f2 | tr -d '\n')
+port=6050
+user="azureuser"
+
+echo "$worker Worker deployment detected"
+echo "Removing ssh key entries"
+echo
+for ((i=2; i < $(( 2 + $worker)); i++))
+do
+    host="127.0.0.1:$(( $port + $i ))"
+    echo "Cleaning up $host"
+    ssh-keygen -R "$host"
+done
+echo 
+echo "Cleanup complete!"
