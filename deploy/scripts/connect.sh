a791f204f5637cf91e2ce63ffae551d9eed593e8
Martin Pouliot <martinp507@gmail.com>
2020-11-13 15:06:51 -0500

    Improved connection script

--- deploy/scripts/connect.sh
+++ deploy/scripts/connect.sh
@@ -1,10 +1,21 @@
-#!/bin/sh
-
+#!/bin/bash
 # Setup the port forwarding from ssh connexion for remote server
+
 ip=$(cat ../creds/ip)
-ssh -L 6052:10.0.0.4:22 \
-    -L 6053:10.0.0.5:22 \
-    -L 6054:10.0.0.6:22 \
-    -L 6055:10.0.0.7:22 \
-    -i ../creds/ssh/id_rsa \
-    azureuser@$ip
+worker=$(cat ../azure.yml | grep "amount_worker:" | xargs | cut -d ' ' -f2 | tr -d '\n')
+port=6050
+user="azureuser"
+
+
+echo "$worker Worker deployment detected"
+
+command="ssh"
+for ((i=2; i < $(( 2 + $worker)); i++))
+do
+    command="${command} -L $(( $port + $i )):10.0.0.$(( $i + 2 )):22"
+done
+command="${command} -i ../creds/ssh/id_rsa $user@$ip"
+echo "Connecting to Bastion"
+echo $command
+
+eval $command
--- deploy/scripts/connect_node.sh
+++ deploy/scripts/connect_node.sh
@@ -1,6 +1,6 @@
-#!/bin/sh
+#!/bin/bash
 
 # Setup the port forwarding from ssh connexion for remote server
 ssh -i ../creds/ssh/id_rsa \
-    -p 605$1 \
+    -p $(( 6050 + $1 )) \
     azureuser@localhost
