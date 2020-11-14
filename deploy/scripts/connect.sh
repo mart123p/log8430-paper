ddd82e0a2af20d471010649c2d6c82728d44428d
Martin Pouliot <martinp507@gmail.com>
2020-11-13 22:01:20 -0500

    Added parameter test_vm

--- deploy/scripts/connect.sh
+++ deploy/scripts/connect.sh
@@ -3,10 +3,18 @@
 
 source common.sh
 
+test_vm=$(cat ../vars/default.yml | grep "test_vm:" | xargs | cut -d ' ' -f2 | tr -d '\n' | awk '{print tolower($0)}')
+
 echo "$worker Worker deployment detected"
+workerCount=$worker
+
+if $test_vm; then
+    echo "Test VM detected"
+    workerCount=$(( $worker + 1 ))
+fi
 
 command="ssh"
-for ((i=2; i < $(( 3 + $worker)); i++))
+for ((i=2; i < $(( 2 + $workerCount)); i++))
 do
     command="${command} -L $(( $port + $i )):10.0.0.$(( $i + 2 )):22"
 done
